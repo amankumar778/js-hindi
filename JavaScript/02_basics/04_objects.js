8c894d69115eb1fa7a6757be49995d0a2a55d274
amankumar778 <amankumar860358@gmail.com>
2026-01-17 02:08:46 +0530

    json talk

--- JavaScript/02_basics/04_objects.js
+++ JavaScript/02_basics/04_objects.js
@@ -19,33 +19,47 @@ const regularUser = {
 // console.log(regularUser.fullName.userFullName.lastName);
 const obj1 = {1:"a", 2:"b"}
 const obj2 = {3:"a", 4:"b"}
-const obj3 = { obj1, obj2}
-console.log(obj3);
+const obj4 = {5:"a", 6:"b"}
+// const obj3 = { obj1, obj2}
+// const obj3 = Object.assign({},obj1,obj2, obj4);
 
-// object literal
+const obj3 ={...obj1,...obj2,...obj4}
+// console.log(obj3);
 
-const mySym = Symbol("key1")
-const jsUser = {
-    name : "Aman",
-    "full name":"Aman kumar",
-    [mySym]:"mykey1",
-    age: 22,
-    email: "amankumar@gmail.com",
-    isLoggedIn : false,
-    lastLoginDay : ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday","sunday"]
-}
-console.log(jsUser.email);
-console.log(jsUser["email"]);
-console.log(jsUser["full name"]);
-console.log(jsUser[mySym])
-jsUser.email ="amankumar@google.com";
-// Object.freeze(jsUser);
+const users = [
+  {
+    id:1,
+    email:"abc@gmail.com"
+  },
+  {
+    d:3
+  }
+]
+users[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
 
-jsUser.greeting = function(){
-    console.log("Hello js user");
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+// Destructuring
+const course = {
+  courseName : "js in hindi",
+  price: "999",
+courseInstructor :"Aman"
 }
-jsUser.greetingTwo = function(){
-    console.log(`Hello jsUser ${this.name}`);
-}
-console.log(jsUser.greeting());
-console.log(jsUser.greetingTwo());
+
+// course.courseInstructor
+const {courseInstructor : instructor} =course 
+console.log(instructor);
+// {
+//   "name":"Aman",
+//   "courserName" : "js in hindi",
+//   "price":"free"
+// }
+[
+  {},
+  {},
+  {}
+]
